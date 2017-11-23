db255ce7432599e510eed36550ed64003fb0595c
Fabrice BUREL <burelfabrice35@gmail.com>
2017-11-23 09:38:07 +0100

    ajout de compétence, modification affichage des info hobbie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 document.getElementById('pompierInfo').style.display='none';
 document.getElementById('athleInfo').style.display='none';
 document.getElementById('handInfo').style.display='none';
-function maskInfo(test){
-  if(document.getElementById(test+'Info').style.display == 'none'){
-    console.log('afficher info pompier');
-    document.getElementById(test+'Info').style.display='block';
-  }else if (document.getElementById(test+'Info').style.display == 'block') {
-    console.log('masquer info pompier');
-    document.getElementById(test+'Info').style.display='none';
+if (window.matchMedia("(max-width: 1280px)").matches) {
+  function displayInfo(hobbie){
+    if(document.getElementById(hobbie+'Info').style.display == 'none'){
+      console.log('afficher info pompier');
+      document.getElementById(hobbie+'Info').style.display='block';
+    }else if (document.getElementById(hobbie+'Info').style.display == 'block') {
+      console.log('masquer info pompier');
+      document.getElementById(hobbie+'Info').style.display='none';
+    }
+  }
+} else {
+  function displayInfo(hobbie){
+    document.getElementById(hobbie+'Info').style.display='block';
+  }
+  function hideInfo(hobbie){
+    console.log('blur de l\'image');
+      document.getElementById(hobbie+'Info').style.display='none';
   }
 }
+
+/*
+}*/
